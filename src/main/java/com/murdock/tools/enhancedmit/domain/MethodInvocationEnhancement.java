db9a6a539f6e35d9c016cb47e0cc64b3804b6353
weipeng2k <weipeng2k@126.com>
2012-12-09 23:15:01 +0800

    dd

--- src/main/java/com/murdock/tools/enhancedmit/domain/MethodInvocationEnhancement.java
+++ src/main/java/com/murdock/tools/enhancedmit/domain/MethodInvocationEnhancement.java
@@ -4,8 +4,11 @@
 package com.murdock.tools.enhancedmit.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import com.murdock.tools.enhancedmit.domain.extension.AfterExecutionHandler;
 import com.murdock.tools.enhancedmit.domain.extension.BeforeExecutionHandler;
@@ -14,20 +17,30 @@ import com.murdock.tools.enhancedmit.enums.CapabilityTypeEnum;
 
 /**
  * <pre>
- * 方法调用增强，一个方法对应一个增强
+ * 方法调用增强
  * 
  * 一个方法的增强，描述了这个方法的：
  * 1. 前置调用前增强列表；
  * 2. 调用完成后增强列表；
  * 3. 异常生成时增强列表。
  * 
+ * 根据对应的方法ID来对应一个增强，这里提到的方法ID不是一个方法的物理本体而是逻辑概念。
+ * 
+ * 增强能够执行方法调用前、方法调用后和异常抛出时的相关操作。
+ * 
  * </pre>
  * 
  * @author weipeng
  */
-public final class MethodInvocationEnhancement {
+public class MethodInvocationEnhancement {
 
-    private static final int             DEFAULT_HANDLERS_SIZE = CapabilityTypeEnum.values().length;
+    private static final int DEFAULT_HANDLERS_SIZE = CapabilityTypeEnum
+	    .values().length;
+    private static Log log = LogFactory.getLog(Handler.class);
+    /**
+     * 方法的ID，这里用该属性来标识一个方法调用增强
+     */
+    private String id;
     /**
      * 前置执行处理器
      */
@@ -35,7 +48,7 @@ public final class MethodInvocationEnhancement {
     /**
      * 后置执行处理器
      */
-    private List<AfterExecutionHandler>  afterExecutionHandlers;
+    private List<AfterExecutionHandler> afterExecutionHandlers;
     /**
      * 异常处理器
      */
@@ -45,54 +58,55 @@ public final class MethodInvocationEnhancement {
      * @param handler
      */
     public final void registerHandler(Handler handler) {
-        if (handler != null) {
-            if (handler instanceof BeforeExecutionHandler) {
-                if (beforeExecutionHandlers == null) {
-                    beforeExecutionHandlers = new ArrayList<BeforeExecutionHandler>(DEFAULT_HANDLERS_SIZE);
-                }
-                beforeExecutionHandlers.add((BeforeExecutionHandler) handler);
-            } else if (handler instanceof AfterExecutionHandler) {
-                if (afterExecutionHandlers == null) {
-                    afterExecutionHandlers = new ArrayList<AfterExecutionHandler>(DEFAULT_HANDLERS_SIZE);
-                }
-                afterExecutionHandlers.add((AfterExecutionHandler) handler);
-            } else if (handler instanceof ExceptionThrownHandler) {
-                if (exceptionThrownHandlers == null) {
-                    exceptionThrownHandlers = new ArrayList<ExceptionThrownHandler>(DEFAULT_HANDLERS_SIZE);
-                }
-                exceptionThrownHandlers.add((ExceptionThrownHandler) handler);
-            }
-        }
+	if (handler != null) {
+	    if (handler instanceof BeforeExecutionHandler) {
+		if (beforeExecutionHandlers == null) {
+		    beforeExecutionHandlers = new ArrayList<BeforeExecutionHandler>(
+			    DEFAULT_HANDLERS_SIZE);
+		}
+		beforeExecutionHandlers.add((BeforeExecutionHandler) handler);
+	    } else if (handler instanceof AfterExecutionHandler) {
+		if (afterExecutionHandlers == null) {
+		    afterExecutionHandlers = new ArrayList<AfterExecutionHandler>(
+			    DEFAULT_HANDLERS_SIZE);
+		}
+		afterExecutionHandlers.add((AfterExecutionHandler) handler);
+	    } else if (handler instanceof ExceptionThrownHandler) {
+		if (exceptionThrownHandlers == null) {
+		    exceptionThrownHandlers = new ArrayList<ExceptionThrownHandler>(
+			    DEFAULT_HANDLERS_SIZE);
+		}
+		exceptionThrownHandlers.add((ExceptionThrownHandler) handler);
+	    }
+	}
     }
 
-    public final List<BeforeExecutionHandler> getBeforeExecutionHandlers() {
-        if (beforeExecutionHandlers == null) {
-            return Collections.emptyList();
-        }
-
-        return beforeExecutionHandlers;
-    }
-
-    public final List<AfterExecutionHandler> getAfterExecutionHandlers() {
-        if (afterExecutionHandlers == null) {
-            return Collections.emptyList();
-        }
-
-        return afterExecutionHandlers;
-    }
-
-    public final List<ExceptionThrownHandler> getExceptionThrownHandlers() {
-        if (exceptionThrownHandlers == null) {
-            return Collections.emptyList();
-        }
-
-        return exceptionThrownHandlers;
+    /**
+     * <pre>
+     * 方法执行前进行执行
+     * 
+     * </pre>
+     * 
+     * @param methodInvocation
+     */
+    public void beforeExecution(MethodInvocation methodInvocation) {
+	if (beforeExecutionHandlers != null) {
+	    for (BeforeExecutionHandler beh : beforeExecutionHandlers) {
+		try {
+		    beh.handle(methodInvocation);
+		} catch (Exception ex) {
+		    // Ignore proceed, just log it.
+		    log.error("handler execution got exception.", ex);
+		}
+	    }
+	}
     }
 
     @Override
     public String toString() {
-        return "MethodInvocationEnhancement [beforeExecutionHandlers=" + beforeExecutionHandlers
-               + ", afterExecutionHandlers=" + afterExecutionHandlers + ", exceptionThrownHandlers="
-               + exceptionThrownHandlers + "]";
+	return "MethodInvocationEnhancement [beforeExecutionHandlers="
+		+ beforeExecutionHandlers + ", afterExecutionHandlers="
+		+ afterExecutionHandlers + ", exceptionThrownHandlers="
+		+ exceptionThrownHandlers + "]";
     }
 }
